7b5d20a82f9a8d5dff0502a010538450012b1ef2
kalyne Crystini <kalyne.crystini@gmail.com>
2023-07-03 12:11:48 -0300

    synopsis screen creation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,163 +5,100 @@ import 'package:flutter/src/widgets/placeholder.dart';
 
 class HomeScreen extends StatelessWidget {
   //const MyWidget({super.key});
-
+  List livros = [
+    {
+      "nome": "É assim que começa",
+      "autor": "Colleen Hoover",
+      "capa": "assets/img1.jpg",
+      "sinopse": """
+          Lily Bloom continua administrando uma floricultura. Seu ex-marido abusivo, Ryle Kincaid, ainda é um    cirurgião. Mas agora os dois estão oficialmente divorciados e dividem a guarda da filha, Emerson.
+          Quando Lily esbarra em Atlas ― com quem não fala há quase dois anos ―, parece que finalmente chegou o momento de retomar o relacionamento da adolescência, já que ele também está solteiro e parece retribuir os sentimentos de Lily. Mas apesar de divorciada, Lily não está exatamente livre de Ryle. Culpando Atlas pelo fim de seu casamento, Ryle não está nada disposto a aceitar o novo relacionamento de Lily, ainda mais com Atlas, o último homem que aceitaria ver perto de sua filha e da ex-esposa.  Alternando entre os pontos de vista de Atlas e Lily, É assim que começa retoma logo após o epílogo de É assim que acaba. Revelando mais sobre o passado de Atlas e acompanhando a jornada de Lily para abraçar a sua segunda chance, no amor enquanto lida com um ex-marido ciumento, É assim que começa prova que ninguém entrega uma leitura mais emocionante do que Colleen Hoover.
+"""
+    },
+    {
+      "nome": "A biblioteca da meia-noite",
+      "autor": "Matt Halg",
+      "capa": "assets/img2.jpg"
+    },
+    {
+      "nome": "As coisas que você só vê quando desacelera",
+      "autor": "Haemin Sunim",
+      "capa": "assets/img3.jpg"
+    },
+    {
+      "nome": "O lado feio do amor",
+      "autor": "Colleen Hoover",
+      "capa": "assets/img4.jpg"
+    },
+    {
+      "nome": "Daisy Jones & the Six",
+      "autor": "Taylor Jenkins Reid",
+      "capa": "assets/img5.jpg"
+    },
+    {
+      "nome": "A garota do lago",
+      "autor": "Charlie Donlea",
+      "capa": "assets/img6.jpg"
+    }
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('home_screen'),
+        title: Text('Meus Livros'),
       ),
 
-      body: GridView.count(
-        //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        padding: const EdgeInsets.all(15),
-        crossAxisCount: 2,
-        mainAxisSpacing: 10,
-        childAspectRatio: 9 / 16,
-        crossAxisSpacing: 15,
-        children: <Widget>[
-          Column(
-            children: [
-              InkWell(
-                onTap: () {
-                  //MaterialPageRoute('sinopseScreen'),
-                  Navigator.of(context).pushNamed('/sinopseScreen');
-                  // Navigator.push(context,
-                  //     MaterialPageRoute(builder: (context) => SinopseScreen()));
-                },
-                child: Container(
-                    child: Image.asset(
-                  'assets/img1.jpg',
-                  fit: BoxFit.cover,
-                  height: 350,
-                  width: 300,
-                )),
-              ),
-              Padding(padding: EdgeInsetsDirectional.only(top: 5)),
-              Text(
-                "É assim que começa",
-                style: PrimaryTitle,
-                maxLines: 1,
-              ),
-              Text(
-                "Colleen Hoover",
-                style: subtitleStyle,
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              Container(
-                  child: Image.asset(
-                'assets/img2.jpg',
-                fit: BoxFit.cover,
-                height: 350,
-                width: 300,
-              )),
-              Padding(padding: EdgeInsetsDirectional.only(top: 5)),
-              Text(
-                "A biblioteca da meia-noite",
-                style: PrimaryTitle,
-                //maxLines: 1,
-              ),
-              Text(
-                "Matt Haig",
-                style: subtitleStyle,
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              Container(
-                  child: Image.asset(
-                'assets/img3.jpg',
-                fit: BoxFit.cover,
-                height: 350,
-                width: 300,
-              )),
-              Padding(padding: EdgeInsetsDirectional.only(top: 5)),
-              Text(
-                "As coisas que você só vê quando desacelera",
-                style: PrimaryTitle,
-                maxLines: 1,
-              ),
-              Text(
-                "Haemin Sunim",
-                style: subtitleStyle,
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              Container(
-                  child: Image.asset(
-                'assets/img4.jpg',
-                fit: BoxFit.cover,
-                height: 350,
-                width: 300,
-              )),
-              Padding(padding: EdgeInsetsDirectional.only(top: 5)),
-              Text(
-                "O lado feio do amor",
-                style: PrimaryTitle,
-                maxLines: 1,
-              ),
-              Text(
-                "Colleen Hoover",
-                style: subtitleStyle,
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              Container(
-                  child: Image.asset(
-                'assets/img5.jpg',
-                fit: BoxFit.cover,
-                height: 350,
-                width: 300,
-              )),
-              Padding(padding: EdgeInsetsDirectional.only(top: 5)),
-              Text(
-                "Daisy Jones & the six",
-                style: PrimaryTitle,
-                maxLines: 1,
-              ),
-              Text(
-                "Taylor Jenkins Reid",
-                style: subtitleStyle,
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              Container(
-                  child: Image.asset(
-                'assets/img6.jpg',
-                fit: BoxFit.cover,
-                height: 350,
-                width: 300,
-              )),
-              Padding(padding: EdgeInsetsDirectional.only(top: 5)),
-              Text(
-                "É assim que começa",
-                style: PrimaryTitle,
-                maxLines: 1,
-              ),
-              Text(
-                "Colleen Hoover",
-                style: subtitleStyle,
-              ),
-            ],
-          ),
-        ],
-      ),
+      body: GridView.builder(
+          itemCount: livros.length,
+          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              childAspectRatio: 9 / 17,
+              crossAxisSpacing: 15),
+          padding: const EdgeInsets.all(15),
+          // crossAxisCount: 2,
+          // mainAxisSpacing: 10,
+          // childAspectRatio: 9 / 17,
+          // crossAxisSpacing: 15,
+          itemBuilder: ((context, index) {
+            var livro = livros[index];
+            return Column(
+              children: [
+                InkWell(
+                  onTap: () {
+                    //MaterialPageRoute('sinopseScreen'),
+                    Navigator.of(context)
+                        .pushNamed('/sinopseScreen', arguments: livro);
+                    // Navigator.push(context,
+                    //     MaterialPageRoute(builder: (context) => SinopseScreen()));
+                  },
+                  child: Container(
+                      child: Image.asset(
+                    livro["capa"],
+                    fit: BoxFit.cover,
+                    height: 350,
+                    width: 300,
+                  )),
+                ),
+                Padding(padding: EdgeInsetsDirectional.only(top: 5)),
+                Text(
+                  livro["nome"],
+                  style: PrimaryTitle,
+                  maxLines: 1,
+                ),
+                Text(
+                  livro["autor"],
+                  style: subtitleStyle,
+                ),
+              ],
+            );
+          })),
 
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
-  TextStyle subtitleStyle = TextStyle(fontSize: 15, color: Colors.grey);
-  TextStyle PrimaryTitle = TextStyle(fontSize: 20, color: Colors.black);
+  TextStyle subtitleStyle = TextStyle(fontSize: 12, color: Colors.grey);
+  TextStyle PrimaryTitle = TextStyle(fontSize: 18, color: Colors.black);
 }
--- lib/screens/sinopse_screen.dart
+++ lib/screens/sinopse_screen.dart
@@ -1,13 +1,25 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class SinopseScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
     // TODO: implement build
-
+    var livro = ModalRoute.of(context)!.settings.arguments as Map;
+    // print(ModalRoute.of(context)!.settings.arguments);
+    // if (ModalRoute.of(context)!.settings.arguments == null) Future.delayed(Duration(milliseconds: 200), () => Navigator.of(context).pushNamed('/'));
     return Scaffold(
         appBar: AppBar(
-      title: Text("Teste"),
-    ));
+      title: Text(livro["nome"]),
+
+      
+    ),
+    body: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(livro["sinopse"], style: TextStyle(fontSize: 16),),
+    ) ,)
+    ;
   }
 }
